94f73edc0daa01ed44617123d732f838c48f56e4
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-23 17:45:50 -0700

    adding commands for nfs mounting

---
+++ reference.sh
@@ -0,0 +1,5 @@
+
+
+sudo mount -t nfs -o nfsvers=4.1 jace-cloud.local:/volumeUSB1/usbshare /home/jace/Jace_Docs
+
+jace-cloud.local:/volumeUSB1/usbshare	/home/jace/Jace_Docs	nfs	nfsvers=4.1,defaults,comment=systemd.automount	0 0
