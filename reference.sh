971b09bfb82bcb2aa7977e39a36af12f3916f13a
Jace Manshadi <jaymanshadi@gmail.com>
2019-09-23 18:12:08 -0700

    adding more references

--- reference.sh
+++ reference.sh
@@ -1,5 +1,11 @@
 
 
+#connect to synology within network from cli
 sudo mount -t nfs -o nfsvers=4.1 jace-cloud.local:/volumeUSB1/usbshare /home/jace/Jace_Docs
 
+#connect to synology within network with /etc/fstab
 jace-cloud.local:/volumeUSB1/usbshare	/home/jace/Jace_Docs	nfs	nfsvers=4.1,defaults,comment=systemd.automount	0 0
+
+#connect to synology remotely on debian
+dav://jacemanshadi.synology.me:5005 -> HTTP
+da6://jacemanshadi.synology.me:5006 -> HTTPS
